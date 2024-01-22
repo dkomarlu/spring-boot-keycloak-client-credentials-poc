a8b53e104c31beca8e00b6852e0de96976e2825c
Dharani <dkomarlu@yahoo.com>
2024-01-21 19:44:56 -0500

    Initial

---
+++ src/main/java/com/einfonet/keycloak/oauth1clientcredentialspoc/Oauth1ClientCredentialsPocApplication.java
@@ -0,0 +1,14 @@
+package com.einfonet.keycloak.oauth1clientcredentialspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth1ClientCredentialsPocApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Oauth1ClientCredentialsPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/einfonet/keycloak/oauth1clientcredentialspoc/Oauth1ClientCredentialsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einfonet.keycloak.oauth1clientcredentialspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth1ClientCredentialsPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
