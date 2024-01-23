2b19e5b4bd06edba1a05662ce4dbbd9c3cd8f7c8
Dharani <dkomarlu@yahoo.com>
2024-01-23 17:50:01 -0500

    Added code call microservice using RestTemplate and WebClient

--- microservice-1/src/main/java/com/einfonet/keycloak/microservice1/controller/Controller1.java
+++ microservice-1/src/main/java/com/einfonet/keycloak/microservice1/controller/Controller1.java
@@ -1,19 +1,55 @@
 package com.einfonet.keycloak.microservice1.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/api/v1")
 public class Controller1 {
 
+    private final RestTemplate restTemplate = new RestTemplateBuilder().build();
+
+    private final WebClient webClient = WebClient.builder().build();
 
     @GetMapping("/microservice1/home")
     @ResponseStatus(HttpStatus.OK)
     public String helloRestTemplate() {
-        return "Hello";
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwt.getTokenValue());
+        ResponseEntity<String> response = restTemplate.exchange(
+               "http://localhost:8084/api/v1/microservice2/home",
+               HttpMethod.GET,
+               new HttpEntity<>(headers),
+               String.class
+       );
+
+        return "Hello from Service 2: " + response.getBody();
+    }
+
+    @GetMapping("/microservice1/callMS2")
+    @ResponseStatus(HttpStatus.OK)
+    public String useWebCleint2CallAnotherService() {
+
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String response = this.webClient
+                .get()
+                .uri("http://localhost:8084/api/v1/microservice2/home")
+                .headers(headers -> headers.setBearerAuth(jwt.getTokenValue()))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return "Hello from Service 2 using WebClient : " + response;
     }
 }
---
+++ microservice-2/src/test/java/com/einfonet/keycloak/microservice2/Microservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einfonet.keycloak.microservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
